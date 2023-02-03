0a995623bb04d384dcd2f9b3497c1c742695289c
Eren <vrsozluk@gmail.com>
2023-02-03 22:32:22 +0300

    Enter function has been added

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const input = document.getElementById('input');
+const input = document.getElementById("input");
 
-input.addEventListener('keyup', ()=> {
-        const examples = document.querySelector('.examples');
-        examples.innerHTML = `
-        <input type="text" id="input" placeholder="Ask something to AI" class="input askai form-control p-3 fixed-bottom">`
-        let blankPage = document.createElement('div');
-        blankPage.innerHTML =  `<div class='text-center pt-5'>Waiting for your question...</div>`
-        examples.appendChild(blankPage)
-       
-})
+input.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    const examples = document.querySelector(".examples");
+    examples.innerHTML = `
+        <input type="text" id="input" placeholder="Ask something to AI" class="input askai form-control p-3 fixed-bottom">`;
+    let blankPage = document.createElement("div");
+    blankPage.innerHTML = `<div class='text-center pt-5'>Waiting for your question...</div>`;
+    examples.appendChild(blankPage);
+  }
+});
